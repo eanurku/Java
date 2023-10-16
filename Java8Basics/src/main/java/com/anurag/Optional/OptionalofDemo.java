b4a43464e59596d6db7f4348e6b55d8a4444145c
Anurag Kumar <anurags62@yahoo.com>
2023-10-16 18:53:35 +0530

    update

--- Java8Basics/src/main/java/com/anurag/Optional/OptionalofDemo.java
+++ Java8Basics/src/main/java/com/anurag/Optional/OptionalofDemo.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public class OptionalofDemo {
     public static void main(String[] args) {
-        //of method is is not for processing null values
+        //of method is  not for processing null values
         Optional<Object> optionalValue = Optional.of(null);
         System.out.println(optionalValue);
     }
